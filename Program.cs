06929ed3ddeab1d93096a858041f9ce692d12a1a
Jim McGee <jmcgee86@gmail.com>
2018-07-21 13:36:07 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FizzBuzzLib;
+
+namespace TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 1; i<=100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+        }
+    }
+}
