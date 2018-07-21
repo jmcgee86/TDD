fcb429a745413357eb03ea11c82209e76a48d3bf
Jim McGee <jmcgee86@gmail.com>
2018-07-21 13:35:47 -0400

    initial commit

---
+++ FizzBuzzLib/FizzBuzzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FizzBuzzLib
+{
+    public class FizzBuzzer
+    {
+       public static string GetValue(int input)
+       {
+           //return "1"; - passes test 1
+            if (input % 3 == 0 && input % 5 == 0)
+                return "FizzBuzz";
+            if(input % 3 == 0)
+                return "Fizz";
+            if (input % 5 ==0)
+                return "Buzz";
+           return input.ToString(); //passes tests 1+2
+       }
+    }
+}
